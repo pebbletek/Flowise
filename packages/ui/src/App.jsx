bae2445c82c14c76e6b0bf0f93a78b0bad937e63
Stuart Hirst <stuart@pebbletek.ai>
2025-05-07 16:42:05 +1000

    feat(pebble): support darkMode URL param for iFrame integration (see /flows integration)

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -1,5 +1,5 @@
-import { useSelector } from 'react-redux'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline, StyledEngineProvider } from '@mui/material'
 
@@ -11,11 +11,27 @@ import themes from '@/themes'
 
 // project imports
 import NavigationScroll from '@/layout/NavigationScroll'
+import { SET_DARKMODE } from '@/store/actions'
 
 // ==============================|| APP ||============================== //
 
 const App = () => {
     const customization = useSelector((state) => state.customization)
+    const dispatch = useDispatch()
+
+    // Pebble Custom: Set dark mode from URL param (see /flows integration)
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search)
+        const darkMode = params.get('darkMode')
+        if (darkMode === '0') {
+            dispatch({ type: SET_DARKMODE, isDarkMode: false })
+            localStorage.setItem('isDarkMode', 'false')
+        } else if (darkMode === '1') {
+            dispatch({ type: SET_DARKMODE, isDarkMode: true })
+            localStorage.setItem('isDarkMode', 'true')
+        }
+        // If darkMode is '2' or missing, do not override user/system preference
+    }, [dispatch])
 
     return (
         <StyledEngineProvider injectFirst>
